fe22788776ef71efb595ae744392d4b9f9f6a9d2
mariam-elkirch <mariamk019@gmail.com>
2022-08-16 21:35:33 +0200

    delete from favourite and display favourite

--- app/src/main/java/com/example/pockomen/favourite/view/FavouriteActivity.kt
+++ app/src/main/java/com/example/pockomen/favourite/view/FavouriteActivity.kt
@@ -3,13 +3,18 @@ package com.example.pockomen.favourite.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pockomen.databinding.ActivityFavouriteBinding
 import com.example.pockomen.displayMovies.view.MainActivity
 import com.example.pockomen.displayMovies.viewmodel.ViewModel
 import com.example.pockomen.favourite.viewmodel.FavouriteAdapter
 import com.example.pockomen.favourite.viewmodel.FavouriteViewModel
+import com.example.pockomen.model.Favourite
+import com.example.pockomen.model.Result
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -39,6 +44,34 @@ class FavouriteActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+        setUpSwipe()
     }
+    fun  setUpSwipe(){
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
 
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+
+                val position = viewHolder.adapterPosition
+
+                val  movie  : Favourite  =  favAdapter.getMovieByPosition(position)
+                val favMovie = Favourite(movie.id,movie.poster_path,movie.title)
+                viewModel.deletePokemon(favMovie)
+               favAdapter.notifyDataSetChanged()
+                Toast.makeText(this@FavouriteActivity, "Movie deleted", Toast.LENGTH_SHORT).show();
+
+            }
+            // at last we are adding this
+            // to our recycler view.
+        }).attachToRecyclerView(binding.favouriteRecyclerView)
+
+    }
 }
